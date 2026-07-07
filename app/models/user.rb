278abea1a12784fe51124f11a06142f6689973f0
Paul Bob <paul.ionut.bob@gmail.com>
2026-07-07 16:38:52 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,6 @@ class User < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
-  def is_developer? = roles[:developer]
-  def is_admin? = roles[:admin]
+  def is_developer? = roles["developer"]
+  def is_admin? = roles["admin"]
 end
